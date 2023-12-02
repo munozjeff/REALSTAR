0646a14089973806c31a589bb3d63cfb3a7a3065
munozjeff <jeffersonsteven231997@gmail.com>
2023-12-01 22:11:16 -0500

    first production commit

--- client/src/components/button/ButtonSeeMore.jsx
+++ client/src/components/button/ButtonSeeMore.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./style.css"
 
-export const ButtonSeeMore = ({color="#FF8300",value="VER MÁS", hover=true}) => {
-  return <input href style={{backgroundColor:color}} className={`button-see-more ${hover && 'grow'}`} type="button" value={value}/>
+export const ButtonSeeMore = ({color="#FF8300",value="VER MÁS", hover=true, to=""}) => {
+  return <a href={to}><input style={{backgroundColor:color}} className={`button-see-more ${hover && 'grow'}`} type="button" value={value}/></a>
 }
